aee28b95e1b674eab4c771be98aa37febc4cc39d
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-08 23:13:16 +1000

    Completed Assignment

--- lib/min.rb
+++ lib/min.rb
@@ -1,13 +1,29 @@
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_min method
-# groceries = {
-#  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+ }
 
 def get_the_min(groceries)
-  #code your solution here!
+  def flatten(groceries)
+    single_array = []
+    array_of_arrays = groceries.values
+    outer_index = 0
+    while outer_index < array_of_arrays.length do
+    inner_index = 0
+    while inner_index < array_of_arrays[outer_index].length do
+      single_array.push(array_of_arrays[outer_index][inner_index])
+    inner_index +=1
+  end
+  outer_index +=1
+  end
+  single_array
+  end
+  p flatten(groceries).min
 end
+
+get_the_min(groceries)
